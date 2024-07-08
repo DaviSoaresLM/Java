31a9f37ffd3e4345a211b88dd2c78f08e9ab2f9e
Davi <davisoaresdasilva1203@gmail.com>
2024-07-08 14:09:27 -0300

    Java

--- EstruturaSequencial/src/exercicios/Exer03.java
+++ EstruturaSequencial/src/exercicios/Exer03.java
@@ -6,8 +6,18 @@ import java.util.Scanner;
 public class Exer03 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         
+        System.out.println("Digite 4 valores: ");
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        int d = scan.nextInt();
+
+        int diferenca = (a * b) - (c * d);
+
+        System.out.printf("Diferença = %d", diferenca);
+
+        scan.close();
 
     }
 
